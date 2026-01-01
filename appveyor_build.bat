5d52020db36959a8acfdc2f22a2c9282950a817c
yuant <imyuant@gmail.com>
2026-01-01 20:13:33 +0800

    Refactor appveyor_build.bat for improved clarity

--- appveyor_build.bat
+++ appveyor_build.bat
@@ -3,7 +3,5 @@ if "%APPVEYOR_REPO_TAG%" == "true" set cmdLine="cd \"$APPVEYOR_BUILD_FOLDER\" &&
 
 set MSYSTEM=MINGW64
 C:\msys64\usr\bin\bash -lc %cmdLine%
-set MSYSTEM=MINGW32
-C:\msys64\usr\bin\bash -lc %cmdLine%
 
 C:\msys64\usr\bin\bash -lc "cd \"$APPVEYOR_BUILD_FOLDER\" && exec ./nginx-package-msys2.sh"
